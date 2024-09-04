5b72bbf69907a55ad26ed9937d7ef8c7e45f0db5
Linus <elfzity@hotmail.com>
2024-09-04 16:23:24 +0800

    Added function to make http call.

--- CallingService/Program.cs
+++ CallingService/Program.cs
@@ -1,3 +1,5 @@
+using System.Net.Http;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -22,6 +24,22 @@ app.MapGet("/helloworld", () =>
     return "Test";
 })
 .WithName("HelloWorld")
+.WithOpenApi();
+
+app.MapGet("/fetch_data", async (string url) =>
+{
+    using var httpClient = new HttpClient();
+
+
+    HttpResponseMessage response = await httpClient.GetAsync(url);
+
+    response.EnsureSuccessStatusCode();
+
+    string responseData = await response.Content.ReadAsStringAsync();
+
+    return Results.Ok(responseData);
+})
+.WithName("FetchData")
 .WithOpenApi();
 
 app.Run();
